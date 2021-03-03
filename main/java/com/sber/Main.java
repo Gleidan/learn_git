5ed331dc94f30853c7df9d6a79078ef46cb2dc03
18887064 <VASavostikov@sberbank.ru>
2021-03-03 15:54:29 +0300

    add method helloUser

--- main/java/com/sber/Main.java
+++ main/java/com/sber/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        helloUser();
+    }
+
+    public static void helloUser() {
+        System.out.println("Hello, my Dear friend");
     }
 }
