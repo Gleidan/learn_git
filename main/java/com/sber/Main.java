d6d96dce40dba3da71cbd06f1c1997b817f69fde
18887064 <VASavostikov@sberbank.ru>
2021-03-03 16:13:09 +0300

    add ageChecking method

--- main/java/com/sber/Main.java
+++ main/java/com/sber/Main.java
@@ -1,5 +1,7 @@
 package com.sber;
 
+import java.util.Scanner;
+
 public class Main {
 
     private static String userName;
@@ -7,10 +9,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ageChecking();
     }
-    
-//    public static void ageChecking() {
-//        System.out.println("Please, enter your age:");
-//
-//    }
+
+    public static void ageChecking() {
+        System.out.println("Please, enter your age:");
+        Scanner scanner = new Scanner(System.in);
+        age = scanner.nextInt();
+        if (age < 18) {
+            System.out.println("Sorry, this site only for adult people");
+        }
+    }
 }
