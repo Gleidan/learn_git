f31b3e7ef6bb1b5adee279e6899bcb3c467b71b1
18887064 <VASavostikov@sberbank.ru>
2021-03-03 16:19:36 +0300

    initiate method signature

--- main/java/com/sber/Main.java
+++ main/java/com/sber/Main.java
@@ -5,4 +5,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void ageChecking() {
+
+    }
+
+    public static void helloUser() {
+
+    }
+
+    public static void createUser() {
+        
+    }
 }
