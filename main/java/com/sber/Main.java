8a1f18d781550aa1ba9a1b260aabd54b72f026b4
18887064 <VASavostikov@sberbank.ru>
2021-03-03 16:28:26 +0300

    add surname field

--- main/java/com/sber/Main.java
+++ main/java/com/sber/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     private static String userName;
     private static int age;
+    private static String surname;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
