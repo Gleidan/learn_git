6091855571974ce3c6884a3d489b2419fe7ca65c
18887064 <VASavostikov@sberbank.ru>
2021-03-03 16:11:19 +0300

    add name and age field

--- main/java/com/sber/Main.java
+++ main/java/com/sber/Main.java
@@ -2,7 +2,15 @@ package com.sber;
 
 public class Main {
 
+    private static String userName;
+    private static int age;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+//    public static void ageChecking() {
+//        System.out.println("Please, enter your age:");
+//
+//    }
 }
